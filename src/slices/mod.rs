f0e0bb3ebf2fa78365229e917da3b7cc59c2e371
Peter Kehl <peter.kehl@gmail.com>
2022-04-03 01:19:50 -0700

    Docs and contructors/conversion functions.

--- src/slices/mod.rs
+++ src/slices/mod.rs
@@ -1,4 +1,10 @@
-/// Param `N` = array/vec size (if Some). Some(0) is allowed (for empty arrays).
+/// Slice/array/vector-based container.
+/// If array-based, the size is fixed at compile time through a const generic param `N`.
+/// If slice-based or vec-based, its size can be any, as given at runtime. But for shared/mutable slice-based instances the size is fixed at instantiation.
+/// Vec-based instances can be resized.
+///
+/// Param `N` = array size (if Some). Some(0) is also allowed (for empty arrays).
+/// Generally, use `N=None` for most `std` purposes, and for instantiation based on a given slice/vector. Use `N=Some(usize)` for owned array-based instantiation (often on stack, or in `non_std`).
 pub trait Slice<'a, T: 'a + Clone + PartialEq, const N: usize>
 where
     Self: 'a,
@@ -17,15 +23,45 @@ where
     fn from_mutable_slice(slice: &'a mut [T]) -> Self;
     fn from_array(array: [T; N]) -> Self;
 
+    /// If `N.is_some()` then this checks the size of the given `vector`. Otherwise it takes `vector` as-is, and it marks `Self` instance as having runtime sized vec.
     #[cfg(all(not(feature = "no_std"), feature = "std"))]
     fn from_vec(vector: Vec<T>) -> Self;
 
     // Constructors setting blank/default vaLues.
     /// Implemented only if T: Copy + Default.
     fn new_with_array() -> Self;
+
     #[cfg(all(not(feature = "no_std"), feature = "std"))]
     /// Implemented only if T: Default.
-    fn new_with_vec() -> Self;
+    fn new_with_vec(size: usize) -> Self;
+
+    // Conversion constructors.
+    fn to_shared_based<'s>(&'s self) -> Self
+    where
+        Self: 's + Sized,
+    {
+        todo!()
+    }
+    fn to_mutable_based<'s>(&'s mut self) -> Self
+    where
+        Self: 's + Sized,
+    {
+        todo!()
+    }
+    // @TODO Would `copy_to_array_based` be a better name?
+    fn to_array_based(&self) -> Self
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+    /// Copy to a new vec and create an instance with it.
+    fn to_vec_based(&self) -> Self
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
 }
 
 /// Const generic param `N` is used by `Slice::Array` only. (However, it makes all variants consume space. Hence:) Suggested for `no_std` only.
@@ -34,10 +70,10 @@ where
 pub enum SliceStorage<'a, T: 'a, const N: usize> {
     Shared(&'a [T]),
     Mutable(&'a mut [T]),
-    /// Owned array. Suggested for no_std only.
+    /// Owned array. Suggested for stack & `no_std`.
     Array([T; N]),
     #[cfg(all(not(feature = "no_std"), feature = "std"))]
-    /// Owned vector. For std only.
+    /// Owned vector. For `std` only.
     Vec(Vec<T>),
 }
 
@@ -102,14 +138,16 @@ impl<'a, T: 'a + Copy + PartialEq + Default, const N: usize> Slice<'a, T, N>
         Self::Mutable(slice)
     }
     fn from_array(array: [T; N]) -> Self {
+        assert!(Some(()).is_some() && N > 0);
         #[cfg(feature = "size_for_owned_only")]
-        assert!(N > 0);
+        assert!(Some(()).is_some() && N > 0);
         // \---> TODO consider const N: Option<usize>, or a custom enum.
         Self::Array(array)
     }
 
     #[cfg(all(not(feature = "no_std"), feature = "std"))]
     fn from_vec(vector: Vec<T>) -> Self {
+        /// @TODO runtime size instead
         #[cfg(feature = "size_for_owned_only")]
         assert!(N > 0);
         assert!(vector.len() == N);
@@ -150,10 +188,11 @@ impl<'s, T: 's + Clone, const N: usize> Clone for SliceStorage<'s, T, N> {
 impl<'s, T: 's + Clone + Copy + Default, const N: usize> crate::abstra::NewLike
     for SliceStorage<'s, T, N>
 {
-    /// Implemented for Array-backed and Vec-backed slice only.
+    /// Implemented for Shared-backed, Array-backed and Vec-backed SliceStorage only.
     fn new_like(&self) -> Self {
         match self {
-            SliceStorage::Shared(_) | SliceStorage::Mutable(_) => {
+            SliceStorage::Shared(slice) => SliceStorage::Shared(slice),
+            SliceStorage::Mutable(_) => {
                 unimplemented!("Can't clone a slice.")
             }
             SliceStorage::Array(_) => SliceStorage::Array([T::default(); N]),
